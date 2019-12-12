594246f9111ad386d06130c1d8a7bd0b70f96ce8
Numairus <stricklandtech@gmail.com>
2019-12-12 06:31:49 -0600

    Add files via upload

---
+++ ChargingPort_0.1.0/settings.lua
@@ -0,0 +1,14 @@
+data:extend({
+
+{
+type = "int-setting",
+name = "ports",
+setting_type = "startup",
+default_value = 4,
+minimum_value = 1,
+maximum_value = 7,
+localised_name = "Roboport robot storage slots",
+localised_description = "Determines number of robot storage vs charge slots: 1 storage = 4 charging"
+}
+
+}) -- data:extend
